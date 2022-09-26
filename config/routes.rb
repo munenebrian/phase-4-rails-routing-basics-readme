1d758e8d12ee6fbe7c7f01e3207d42d0528a068f
munenebrian <njerubrianmunene@gmail.com>
2022-09-26 08:39:33 +0300

    learning

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'cheeses', to: 'cheeses#index'
+  # for details on the dsl available within this file, see https://guides.rubyonrails.org/routing.html
 end
